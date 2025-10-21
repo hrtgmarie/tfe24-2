abb580733fd0c62434102806e3b823c10dec28ff
hrtgmarie <hartig.marie-it24@it.dhbw-ravensburg.de>
2025-10-21 14:04:03 +0200

    exercise-03 added vector with random fillings

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -4,6 +4,9 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+#include <random>
+#include <vector>
+
 auto main(int argc, char **argv) -> int
 {
     /**
@@ -11,11 +14,10 @@ auto main(int argc, char **argv) -> int
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
     CLI::App app{PROJECT_NAME};
+    auto count = 20;
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        // Option "count" mit Default 20
-        std::size_t count = 20;
         app.add_option("-c,--count", count, "Anzahl der Zufallswerte")->default_val("20");
         app.parse(argc, argv);
     }
@@ -30,6 +32,21 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("count = {}\n", count);
+
+    // vector the size of count
+    // fill the vector with random values between 1 and 100
+    std::vector<int> values;
+    values.resize(count);
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(1, 100);
+
+    for (auto &v : values)
+    {
+        v = dist(rng);
+    }
 
     return 0; /* exit gracefully*/
 }
